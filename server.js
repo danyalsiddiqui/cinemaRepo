6e637e75aa8954ba8e4e20223381fcd50486a879
danyalsiddiqui <danyalxiddiqui@gmail.com>
2016-12-28 19:27:33 +0500

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.listen(port);
 
 app.get('/listData', function (req, res) {
 
-  sql.connect("Server=691ac767-73fd-41bc-99de-a65f007b6261.sqlserver.sequelizer.com;Database=db691ac76773fd41bc99dea65f007b6261;User ID=lkagkpksojppoyei;Password=iL6JhvLRbrPfKwQCCDQqbvmdH6G7PGetRQpM2iXfUsng4sgjNhgdq4rVy2bkWMbA;").then(function() {
+  sql.connect("Server=691ac767-73fd-41bc-99de-a65f007b6261.sqlserver.sequelizer.com;Database=db691ac76773fd41bc99dea65f007b6261;User ID=lkagkpksojppoyei;Password=8RncMgsmVu6eu2VCvRsSGQJPgNpcK6YykEYnvRbFD8YJcyLibYVLAqEqgE48YpHY;").then(function() {
     // Query
 
     new sql.Request().query('select top(2) * from [db691ac76773fd41bc99dea65f007b6261].[dbo].[ScheduleData]').then(function(recordset) {
